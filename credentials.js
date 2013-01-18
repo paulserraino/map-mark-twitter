e03c35cf15bc3536fc7f83337b55459673067605
paulserraino <pserraino99@gmail.com>
2013-01-17 23:28:39 -0600

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 module.exports = {
-	consumer_key: 'Usxahji19kTe4XHZwydg',
-	consumer_secret: 'bC3sbBcgOmWNFPJXolNCqLZyD2zNSc0hQkaT0PIaQ',
-	access_token_key: '869544151-x15nbTWvE7YjmIeNpKTDinwZaRPyHsUV66Udpxz0',
-	access_token_secret: 'y73FGqLvphi62IH6DoF77Put8ctBX2TFgGHYbLfLuk'
+	consumer_key: '#',
+	consumer_secret: '#',
+	access_token_key: '#',
+	access_token_secret: '#'
 };
 
